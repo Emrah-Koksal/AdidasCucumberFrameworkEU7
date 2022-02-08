156256a8c462e303948a7af58575589d7559182e
MikeJ77 <mikejohnson3505@gmail.com>
2022-02-08 11:12:20 +0300

    BasePage commit

---
+++ src/test/java/com/demoblaze/step_definitions/PurchaseSteps.java
@@ -0,0 +1,21 @@
+package com.demoblaze.step_definitions;
+
+import com.demoblaze.utilities.ConfigurationReader;
+import com.demoblaze.utilities.Driver;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+
+public class PurchaseSteps {
+
+    @Given("User is on the home page")
+    public void user_is_on_the_home_page() {
+        Driver.get().get(ConfigurationReader.get("demoblaze.url"));
+
+    }
+
+    @When("User adds {string} {string}")
+    public void user_adds(String product, String category) {
+
+    }
+
+}
